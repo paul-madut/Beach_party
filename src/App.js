2c2e7959cead5fa1756a88d10670f6014e2b1180
Paul Madut <54954818+paul-madut@users.noreply.github.com>
2023-02-09 18:07:04 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import BeachDetails from "./components/BeachDetails/BeachDetails";
+import Map from "./components/Map/Map";
+
+import { CssBaseline,Grid } from "@material-ui/core";
+function App() {
+  return (
+    <>
+    <CssBaseline/>
+    <Header/>
+    <Grid container spacing={3} style={{width: '100%'}}>
+          <Grid item xs={12} md={4}>
+            <List/>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Map/>
+          </Grid>
+    </Grid>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/List/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const List = () => {
+    return(
+        <>
+        <h1>This is a Lsit</h1>
+        </>
+    )
+}
+
+export default List
