7e9efc2ca95ba657bc3b54be09879b1aa31a812a
Paul Madut <54954818+paul-madut@users.noreply.github.com>
2023-02-10 00:21:10 -0500

    Added Working Map

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { CircularProgress,Grid,Typography,InputLabel,MenuItem,FormControl,Select } from "@material-ui/core";
 
+import useStles from './styles'
 const List = () => {
     return(
         <>
-        <h1>This is a Lsit</h1>
+
         </>
     )
 }
