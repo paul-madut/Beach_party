af2e647db524fb774ceee7a4ac0d8072e0a232d8
paul-madut <paul.madut@mths.ca>
2023-02-25 23:12:19 -0500

    Squashed commit of the following:
    commit 3e84be3896c9a59ca5efde1a1e126df7abc5aa71
    Author: paul-madut <paul.madut@mths.ca>
    Date:   Sat Feb 25 23:07:58 2023 -0500
    
        Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,24 +19,3 @@ const Header = () => {
 }
 
 export default Header
-
-        // <AppBar position ="static">
-        //     <Toolbar className={classes.toolbar}>
-        //         <Typography variant="h5" className={classes.title} >
-        //             Beach Party
-        //         </Typography>
-        //         <Box display="flex"></Box>
-        //             <Typography variant="h5" className={classes.title} >
-        //                 Clean Ups Have Never Been More Fun
-        //             </Typography>
-        //             {/*<Autocomplete> */}
-        //                 <div className={classes.search}>
-        //                     <div className={classes.searchIcon}>
-        //                         <Search></Search>
-        //                     </div>
-        //                 <InputBase placeholder="Where to?" classes={{root:classes.inputRoot,input:classes.inputInput}}/>
-        //                 </div>
-        //             {/*</Autocomplete> */}
-        //     </Toolbar>
-
-        // </AppBar>
