2263a626d213bffb1ae28e51fd72969511a3250b
paul-madut <paul.madut@mths.ca>
2023-02-26 00:29:14 -0500

    Login Page Styling done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -8,8 +8,33 @@ const Login = () => {
 
     return(
         <>
-        <div className="background bg-[url('https://static.vecteezy.com/system/resources/thumbnails/003/439/682/original/cartoon-background-simple-beach-free-video.jpg')]">
-            hi
+        <div className=" justify-center flex align-middle bg-cover h-screen background bg-[url('https://static.vecteezy.com/system/resources/thumbnails/003/439/682/original/cartoon-background-simple-beach-free-video.jpg')]">
+            <div className="login__panel h-1/2 w-1/3 bg-white shadow-2xl self-center mb-20 rounded-3xl">
+            <form class=" px-8 py-6">
+            <div class="mb-4">
+        <label class="block text-gray-700 font-bold mb-2" for="username">
+          Username
+        </label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 font-bold mb-2" for="password">
+          Password
+        </label>
+        <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+        <p class="text-red-500 text-xs italic">Please choose a password.</p>
+      </div>
+      <div class="flex items-center justify-between">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          Sign In
+        </button>
+        <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+          Forgot Password?
+        </a>
+      </div>
+    </form>
+                
+            </div>
         </div>
         </>
     )
