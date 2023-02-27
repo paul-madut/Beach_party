1f906b0577fd368488d4b87a2e21c5e3eb4bfeee
paul-madut <paul.madut@mths.ca>
2023-02-26 19:05:46 -0500

    merge

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
-<<<<<<< HEAD
 import { react } from '@babel/types';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './Redux/Store'
 import { Provider } from 'react-redux'
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
->>>>>>> 7164b9199a262599153962c83704ebac05dd7c3d
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< HEAD
     <Provider store={store}>
         <App />
     </Provider>
-=======
-    <App />
->>>>>>> 7164b9199a262599153962c83704ebac05dd7c3d
 );
 
